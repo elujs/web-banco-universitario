7b64b3363ddc59d997ac08465a71a27506f454c1
Willian Molina <1001.28548270.ucla@gmail.com>
2026-05-20 12:17:50 -0400

    se creo el componente hero.vue y se agrego a la vista HomeInstitucional.vue, ademas se actualizo el package-lock.json con las dependencias necesarias para el proyecto.

--- src/views/HomeInstitucional.vue
+++ src/views/HomeInstitucional.vue
@@ -8,3 +8,17 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue'
 </script>
+
+<template>
+  <div class="font-sans text-gray-900">
+    <Navbar />
+    <main>
+      <Hero />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+import Hero from '@/components/Hero.vue'
+</script>
