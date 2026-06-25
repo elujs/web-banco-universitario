7c2492cb3d2e0058e80e3c4c4e9710dd4c5ef0ce
Aixsmar <carpioaixsmar@gmail.com>
2026-06-25 13:07:59 -0400

    Creacion de carpeta auth y reubicacion de UserRegister

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeInstitucional from '../views/landing/HomeInstitucional.vue'
 import bancaenlinea from '../views/banca/Bancaenlinea.vue'
-import UserRegister from '../views/UserRegister.vue'
+import UserRegister from '../views/auth/UserRegister.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
