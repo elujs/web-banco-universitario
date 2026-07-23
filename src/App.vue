30f6faf27ccac30cf0695452cf575a1d6ce54ea2
Aixsmar <carpioaixsmar@gmail.com>
2026-07-22 23:33:09 -0400

    efecto entre cambios de seccion

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
-  <RouterView />
+  <router-view v-slot="{ Component }">
+    <transition name="slide" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
 
-<script setup>
-import { RouterView } from 'vue-router'
-</script>
+<style>
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.2s ease-in-out;
+}
+
+.slide-enter-from {
+  opacity: 0;
+  transform: translateX(15px); /* Entra deslizándose sutilmente desde la derecha */
+}
+
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-15px); /* Sale deslizándose hacia la izquierda */
+}
+</style>
